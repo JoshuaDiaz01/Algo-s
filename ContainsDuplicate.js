1144c8699170c3264760d6f7e7bde75974d074d0
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-09-26 10:41:04 -0700

    second solution

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -1,18 +1,41 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
- const nums1 = [1,2,3,1]
- const nums2 = [1,2,3,]
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+//  const nums1 = [1,2,3,1]
+//  const nums2 = [1,2,3,]
 
-var containsDuplicate = (nums) => {
-    for (let right = 0; right < nums.length; right ++){
-        for (let left = 0; left < right; left ++){
-            const isDuplicate = nums[left] === nums[right];
-            if(isDuplicate) return true;
+// var containsDuplicate = (nums) => {
+//     for (let right = 0; right < nums.length; right ++){
+//         for (let left = 0; left < right; left ++){
+//             const isDuplicate = nums[left] === nums[right];
+//             if(isDuplicate) return true;
+//         }
+//     }
+//     return false
+// }
+
+// console.log(containsDuplicate(nums2))
+
+
+//hash table allows us to store keys and values 
+//hash tables can be good for not having two for loops
+let test1 = [1,2,3,4,2]
+let test2 = [1,2,3,4,5]
+
+var containsDuplicate = function (nums) {
+    //initialize hash table to store keys and values
+    let hTable = {}
+    for (let i = 0; i < nums.length; i++){
+        //have we seen this number at i before?
+        if(nums[i] in hTable){
+            return true
         }
+        //if we havent seen the number it will get stored in hash table as 1:1
+        hTable[nums[i]] = 1;
     }
     return false
 }
 
-console.log(containsDuplicate(nums2))
+console.log(containsDuplicate(test1))
+console.log(containsDuplicate(test2))
