946af6e3592e76b7e7374ebf008d0fb6863e823b
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-09-28 14:40:38 -0700

    encode and decode strings

---
+++ encodeStrings.js
@@ -0,0 +1,22 @@
+//design algo to encode a list of strings to a string. 
+//encoded string is then sent over the network and is decoded back to the og list of strings
+
+//the output must be same as input
+var strs = ["hello", "world"]
+//encode a list of strings into a signle string
+var encode = function (strs) {
+    //if empty strings return null
+    if (!strs.length) return null
+
+    return strs.join("-encodeStr");
+}
+
+//decode a single string to a list of strings
+var decode = function (s) {
+    //if the strs are empty 
+    if (s === null) return [];
+    //this will split the two strs between the -encode
+    return s.split("-encodeStr")
+}
+
+console.log(decode(encode(strs)))
---
+++ productArray.js
@@ -0,0 +1,35 @@
+//ex https://www.youtube.com/watch?v=XkKmR7RANLY
+nums = [1,2,3,4]
+//output : [24,12,8,6]
+//the outcome should be the product of all other numbers nultiplied except its own index
+
+function productExceptItself(nums) {
+    //create a left array
+    const left = new Array(nums.length).fill(0);
+    //set left at 0 to 1
+    left[0] = 1
+
+    //create a right array
+    const right = new Array(nums.length).fill(0);
+    //right most value to be 1
+    right[right.length-1] = 1
+
+
+    //start at 1 because there is nothing at left side
+    for (let i = 1; i< nums.length; i++){
+        left[i] = nums[i-1]*left[i-1];
+    }
+    //the right.length-2 will position us one off the index
+    for (let i =right.length -2; i >= 0; i--){
+        right[i] = nums[i+1]*right[i+1]
+    }
+
+    //lastly have to combine
+    for (let i = 0; i< nums.length; i++){
+        nums[i] = left[i]*right[i];
+    }
+
+    return nums;
+}
+
+console.log(productExceptItself(nums))
