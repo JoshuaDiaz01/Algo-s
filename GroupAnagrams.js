cd1dcda1a6743b926d79b23c48f394acfe5d4959
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-09-29 14:19:23 -0700

    redo for refresh

--- GroupAnagrams.js
+++ GroupAnagrams.js
@@ -28,23 +28,43 @@ strs = ["eat","tea","tan","ate","nat","bat"];
 // console.log(groupAnagrams(strs))
 
 //redoing for foundation
-var groupAnagrams = function (strs){
-    //sorting the strings
-    const sortedStrs = strs.map(word => word.split('').sort().join(''))
-    //initializing hash
-    const hash = {}
+// var groupAnagrams = function (strs){
+//     //sorting the strings
+//     const sortedStrs = strs.map(word => word.split('').sort().join(''))
+//     //initializing hash
+//     const hash = {}
 
-    //looping through strs
-    for(let i=0; i<strs.length; i++){
-        //if it is not an anagram, leave it alone
-        if(!hash[sortedStrs[i]]){
-            hash[sortedStrs[i]] = [strs[i]];
+//     //looping through strs
+//     for(let i=0; i<strs.length; i++){
+//         //if it is not an anagram, leave it alone
+//         if(!hash[sortedStrs[i]]){
+//             hash[sortedStrs[i]] = [strs[i]];
+//         }
+//         //if it is an anagram we want to replace the strs at i with the sorted strings at i
+//         else{
+//             hash[sortedStrs[i]].push(strs[i])
+//         }
+        
+//     }
+//     return Object.values(hash)
+// }
+
+var groupAnagrams = function (strs) {
+    //sort the strings 
+    const sortedStrs = strs.map(word => word.split('').sort().join(''))
+    // initialize the hash
+    hash = {}
+
+    //loop through hash
+    for (let i = 0; i<strs.length; i++){
+        //if there is no anagram, set the strings to i
+        if(!hash[strs[i]]){
+            hash[sortedStrs[i]] = [strs[i]]
         }
-        //if it is an anagram we want to replace the strs at i with the sorted strings at i
+        //if it is anagram set strs at i to sorted strs i
         else{
             hash[sortedStrs[i]].push(strs[i])
         }
-        
     }
     return Object.values(hash)
 }
