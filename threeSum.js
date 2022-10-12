8e116f6bc1c3e72a2be3bb510b76839b223490ca
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-10-12 15:42:17 -0700

    refresh

--- threeSum.js
+++ threeSum.js
@@ -58,29 +58,56 @@ let nums = [-1,0,1,2,-1,4]
 
 
 //refresher
-var threeSum = function (nums){
-    //first sort the nums to make sure we dont get duplicates
-    nums.sort();
-    //initiate the empty return array
-    const results = []
+// var threeSum = function (nums){
+//     //first sort the nums to make sure we dont get duplicates
+//     nums.sort();
+//     //initiate the empty return array
+//     const results = []
 
-    //must have three for loops to be able to compare
-    //in order to not get any duplicates we must have if condition 
-    for (let i = 0; i <nums.length; i++){
-        //this if condition will check if there are duplicates, since nums[i-1] will return the num before num[i]
-        if (i>0 && nums[i] === nums[i-1]) continue
-        for(let j= i+1; j<nums.length; j++){
-            if(j>i+1 && nums[j] === nums[j-1]) continue
-            for (let k = j+1; k<nums.length; k++){
-                if(k>j+1 && nums[k] === nums[k-1])continue
-                //can now see if the sum of the variables == 0
-                if (nums[i]+ nums[j]+ nums[k] === 0){
-                    results.push([nums[i], nums[k], nums[j]])
+//     //must have three for loops to be able to compare
+//     //in order to not get any duplicates we must have if condition 
+//     for (let i = 0; i <nums.length; i++){
+//         //this if condition will check if there are duplicates, since nums[i-1] will return the num before num[i]
+//         if (i>0 && nums[i] === nums[i-1]) continue
+//         for(let j= i+1; j<nums.length; j++){
+//             if(j>i+1 && nums[j] === nums[j-1]) continue
+//             for (let k = j+1; k<nums.length; k++){
+//                 if(k>j+1 && nums[k] === nums[k-1])continue
+//                 //can now see if the sum of the variables == 0
+//                 if (nums[i]+ nums[j]+ nums[k] === 0){
+//                     results.push([nums[i], nums[k], nums[j]])
+//                 }
+//             }
+//         }
+//     }
+//     return results 
+// }
+
+// console.log(threeSum(nums))
+
+function threeSum(nums) {
+    //find combination of three nums without using duplicates
+
+    //sort the numbers to use the if condition and continue
+    nums.sort();
+
+    const results = [];
+
+    for(let i = 0; i<nums.length; i++){
+        //this will check for duplicates and skip this iteration if it is a duplicate
+        if(i>0 && nums[i] == nums[i-1]) continue
+        for(let j = i+1; j<nums.length; j++){
+            if(j>i+1 && nums[j] == nums[j-1])continue
+            for(let k = j+1; k<nums.length; k++){
+                if(k>j+1 && nums[k] == nums[k-1]) continue
+                //final condition t chekc in nums at i,j,k ===0
+                if(nums[i]+nums[k]+nums[j] === 0){
+                    results.push([nums[i],nums[k], nums[j]])
                 }
             }
         }
     }
-    return results 
+    return results
 }
 
-console.log(threeSum(nums))
+ console.log(threeSum(nums))
