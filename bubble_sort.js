8702d729be2d60c68ce66af78d18e79fc2d43cb3
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-12-19 17:30:39 -0800

    bubble sort

--- bubble_sort.js
+++ bubble_sort.js
@@ -11,6 +11,7 @@
   For every pair of adjacent indices, swap them if the item on the left is larger than the item on the right until array is fully sorted
 */
 
+
 const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
